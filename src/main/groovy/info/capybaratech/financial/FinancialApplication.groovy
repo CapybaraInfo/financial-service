e35eab0280f69f546d9471406d6e6636e296240c
Carlos3108 <c.silva3108@hotmail.com>
2024-06-20 21:14:54 -0300

    First commit

---
+++ src/main/groovy/info/capybaratech/financial/FinancialApplication.groovy
@@ -0,0 +1,13 @@
+package info.capybaratech.financial
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class FinancialApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(FinancialApplication, args)
+	}
+
+}
